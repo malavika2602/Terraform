9da7d503f13f989309ad63adfe03080bf157446d
malavika2602 <124796404+malavika2602@users.noreply.github.com>
2023-12-25 15:14:10 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.4.0"
+    }
+  }
+
+  required_version = ">= 0.12"
+}
+
+
 provider "aws"{
     region="ap-south-1"
     alias = "env"
